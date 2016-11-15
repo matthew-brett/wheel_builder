4c1ec84a9e76360bc5ecad7d47aeb49570ab0faf
Stefan van der Walt <stefanv@berkeley.edu>
2016-11-15 12:14:18 -0800

    Try Matthew's netcdf wheels

--- config.sh
+++ config.sh
@@ -4,7 +4,9 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    build_hdf5
+    echo "Pre build installs"
+    CDN_URL=https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rackcdn.com
+    pip install -f $CDN_URL netCDF4
 }
 
 function run_tests {
